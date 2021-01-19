837421a0dc7ddbf1a06b7e8280cca0ff2152eaa1
blackbracken <me@bracken.black>
2021-01-19 19:32:52 +0900

    say helloworld

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 extern crate ncurses;
 
-use ncurses::{addstr, endwin, initscr, refresh, getch};
+use ncurses::{addstr, attrset, COLOR_PAIR, curs_set, endwin, getch, initscr, keypad, mvaddstr, noecho, refresh, start_color, stdscr, use_default_colors};
+use ncurses::CURSOR_VISIBILITY::CURSOR_INVISIBLE;
+
+use crate::graphics::{prepare_colors, TetrisColor, ViewColor};
+
+mod graphics;
 
 fn main() {
-    initscr();
+    if let None = init_ncurses() {
+        eprintln!("Failed to initialize");
+        return;
+    }
 
-    addstr("hello, world!");
+    attrset(ViewColor::WindowFrame.to_attr());
+    mvaddstr(3, 30, "Hello, world!");
 
     refresh();
-
     getch();
 
     endwin();
+}
+
+fn init_ncurses() -> Option<()> {
+    initscr();
+
+    curs_set(CURSOR_INVISIBLE)?;
+
+    noecho();
+    keypad(stdscr(), true);
+    start_color();
+    use_default_colors();
+
+    prepare_colors();
+
+    Some(())
 }
