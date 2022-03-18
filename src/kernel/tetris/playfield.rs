36fe19227e12fc9dad87f4ee5463bb72fb082b2c
blackbracken <me@bracken.black>
2022-03-19 02:54:57 +0900

    wip

--- src/kernel/tetris/playfield.rs
+++ src/kernel/tetris/playfield.rs
@@ -1,29 +1,53 @@
+use derive_new::new;
+
 use super::mino::{block::Block, spin::MinoRotation, Mino, WhichMold};
 use crate::kernel::xy_pos::I16XYPos;
 
 const FIELD_UNIT_WIDTH: usize = 10;
 const FIELD_UNIT_HEIGHT: usize = 20; // NOTE: 21行目以降を表示しないので20で済ませている
 
-type Board = [[Option<Block>; FIELD_UNIT_WIDTH]; FIELD_UNIT_HEIGHT];
+#[derive(Clone)]
+struct Board([[Option<Block>; FIELD_UNIT_WIDTH]; FIELD_UNIT_HEIGHT]);
+
+impl Board {
+    pub fn demonstrated(mut self, drop: Drop) -> Self {
+        let mold = drop.mino.mold;
+
+        fn f<const N: usize>() {
+            for (y, line) in mold.matrix.iter().enumerate() {
+                for (x, exists) in line.iter().enumerate() {
+                    board[y][x] = Some(self.dropping.block).filter(|_| *exists);
+                }
+            }
+        }
+
+        self
+    }
+
+
+}
+
+#[derive(new)]
+struct Drop {
+    pub mino: Mino,
+    pub pos: I16XYPos,
+    pub rot: MinoRotation,
+}
 
 /// 盤面の状態を表現する.
 pub struct Playfield {
     /// 盤面上で配置が確定したミノブロック
     pub confirmed: Board,
-    pub dropping: Mino,
-    pub dropping_pos: I16XYPos,
-    pub dropping_rot: MinoRotation,
+    pub drop: Drop,
 }
 
 impl Playfield {
     pub fn new(dropping: Mino) -> Playfield {
-        let confirmed = [[None; FIELD_UNIT_WIDTH]; FIELD_UNIT_HEIGHT];
+        let confirmed = Board([[None; FIELD_UNIT_WIDTH]; FIELD_UNIT_HEIGHT]);
 
         Playfield {
             confirmed,
-            dropping,
-            dropping_pos: (4_i16, 1_i16).into(),
-            dropping_rot: MinoRotation::Clockwise,
+            drop: Drop::new(dropping, (4_i16, 1_i16).into(), MinoRotation::Clockwise),
         }
     }
 
